0ed08c0837b1412f789994d24780c2658935553f
prtpj1 <prtpj1@gmail.com>
2025-05-01 21:22:18 -0300

    🌟 feat: adiciona o componente SubscribeForm ➡️o componente substitui o form que estava na Home page ➡️impelmenta o focus automatico no primeiro campo do formulario

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,13 @@
 import Chat from "@/components/chat/Chat";
 import styles from "./page.module.css";
 import Link from "next/link";
+import SubscribeForm from "@/components/subscribeForm/SubscribeForm";
 
 export default function Home() {
 	return (
 		<div className={styles.page}>
 			<main className={styles.main}>
-				<form
-					className={styles.formContainer}
-					aria-label="Formulário de inscrição"
-				>
-					<h1 className={styles.formTitle} id="subscribe-title">
-						SEJA FURIA
-					</h1>
-					<fieldset className={styles.formInputContainer}>
-						<legend className={styles.visuallyHidden}>
-							Dados para inscrição
-						</legend>
-						<div className={styles.formInputWrapper}>
-							<label className={styles.visuallyHidden} htmlFor="name">
-								Nome
-							</label>
-							<input
-								className={styles.formInput}
-								type="text"
-								id="name"
-								name="name"
-								placeholder="Nome"
-								required
-							/>
-						</div>
-						<div className={styles.formInputWrapper}>
-							<label className={styles.visuallyHidden} htmlFor="email">
-								Email
-							</label>
-							<input
-								className={styles.formInput}
-								type="email"
-								id="email"
-								name="email"
-								placeholder="Email"
-								required
-							/>
-						</div>
-					</fieldset>
-					<fieldset className={styles.formBtnContainer}>
-						<legend className={styles.visuallyHidden}>
-							Ações do formulário
-						</legend>
-						<button
-							className={`${styles.btn} ${styles.btnSubmit}`}
-							type="submit"
-						>
-							INSCREVA-SE
-						</button>
-						<Link
-							className={`${styles.btn} ${styles.btnWebsite}`}
-							href="https://www.furia.gg/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Visite nosso site
-						</Link>
-					</fieldset>
-				</form>
+				<SubscribeForm />
 			</main>
 			<section className={styles.chatContainer}>
 				<Chat />
---
+++ src/components/subscribeForm/SubscribeForm.tsx
@@ -0,0 +1,64 @@
+"use client";
+import Link from "next/link";
+import styles from "./subscribeForm.module.css";
+import { useEffect, useRef, type FC } from "react";
+
+export default function SubscribeForm() {
+	const nameInputRef = useRef<HTMLInputElement>(null);
+
+	useEffect(() => {
+		nameInputRef.current?.focus();
+	}, []);
+
+	return (
+		<form className={styles.formContainer} aria-labelledby="subscribe-title">
+			<h1 className={styles.formTitle} id="subscribe-title">
+				SEJA FURIA
+			</h1>
+			<fieldset className={styles.formInputContainer}>
+				<legend className={styles.visuallyHidden}>Dados para inscrição</legend>
+				<div className={styles.formInputWrapper}>
+					<label className={styles.visuallyHidden} htmlFor="name">
+						Nome
+					</label>
+					<input
+						className={styles.formInput}
+						type="text"
+						id="name"
+						name="name"
+						ref={nameInputRef}
+						placeholder="Nome"
+						required
+					/>
+				</div>
+				<div className={styles.formInputWrapper}>
+					<label className={styles.visuallyHidden} htmlFor="email">
+						Email
+					</label>
+					<input
+						className={styles.formInput}
+						type="email"
+						id="email"
+						name="email"
+						placeholder="Email"
+						required
+					/>
+				</div>
+			</fieldset>
+			<fieldset className={styles.formBtnContainer}>
+				<legend className={styles.visuallyHidden}>Ações do formulário</legend>
+				<button className={`${styles.btn} ${styles.btnSubmit}`} type="submit">
+					INSCREVA-SE
+				</button>
+				<Link
+					className={`${styles.btn} ${styles.btnWebsite}`}
+					href="https://www.furia.gg/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Visite nosso site
+				</Link>
+			</fieldset>
+		</form>
+	);
+}
