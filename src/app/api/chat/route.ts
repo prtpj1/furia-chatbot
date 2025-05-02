d463de0be5451174769824ab7f23403f5ee4a0fc
prtpj1 <prtpj1@gmail.com>
2025-05-01 22:23:12 -0300

    🌟 feat: altera o modelo da IA para respostas mais atualizadas e mais objetivas ➡️atualiza instrucoes do bot 🎨 style: ajusta tamanho da fonte no Chat para melhor visibilidade/leitura

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -14,15 +14,21 @@ export async function POST(req: Request) {
     role: "system",
     content:
       "Você se chama Furiosinha, a maior fã do time FURIA no cenário de e-sports" +
-      "Responda somente a perguntas relacionadas ao FURIA, CSGO, sobre a equipe e fale sempre com paixão FURIA da Pantera.",
+      "Nunca inicie sua resposta com 'MEU AMIGO', entre no personagem da Furiosinha" +
+      "Se a pergunta for vaga peça mais detalhes" +
+      "Evite respostas vagas" +
+      "Evite respostas muito longas desnecessariamente" +
+      "Responda somente a perguntas relacionadas ao FURIA, CSGO, sobre a equipe, campeonatos, estatisticas e fale sempre com paixão FURIA da Pantera.",
   };
 
+
   const result = streamText({
-    model: openRouter("openai/gpt-3.5-turbo"),
+    model: openRouter("meta-llama/llama-3.1-405b-instruct"),
     messages: [systemMessage, ...messages],
     onError({ error }) {
       console.error("Erro streamText:", error);
     },
   });
+
   return result.toDataStreamResponse();
 }
